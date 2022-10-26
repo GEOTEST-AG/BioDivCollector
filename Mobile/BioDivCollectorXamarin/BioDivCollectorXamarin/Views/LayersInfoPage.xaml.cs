3389e66a886d2c910f0b7b1a5de58e32672fc3c0
dawes <nicholas.dawes@geotest.ch>
2022-10-26 10:06:38 +0200

    Information on how to add local layers

---
+++ Mobile/BioDivCollectorXamarin/BioDivCollectorXamarin/Views/LayersInfoPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BioDivCollectorXamarin.Views
+{
+    public partial class LayersInfoPage : ContentPage
+    {
+        public LayersInfoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
