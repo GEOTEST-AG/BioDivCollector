d272a8a414f0c09b36ff2a588c6cea4f9bc0e5cd
dawes <nicholas.dawes@geotest.ch>
2023-01-17 21:24:40 +0100

    missing await added so that ID is taken from project, not task

--- Mobile/BioDivCollectorXamarin/BioDivCollectorXamarin/Models/MapModel.cs
+++ Mobile/BioDivCollectorXamarin/BioDivCollectorXamarin/Models/MapModel.cs
@@ -1015,7 +1015,7 @@ namespace BioDivCollectorXamarin.Models
                     offlineLayers.TryGetValue(layertitle, out ILayer layer);
                     var lay = new DatabaseModel.Layer();
                     var path = dirPath + "/" + layertitle;
-                    var proj = Project.FetchProject(App.CurrentProjectId);
+                    var proj = await Project.FetchProject(App.CurrentProjectId);
                     await BioDivCollectorXamarin.Models.DatabaseModel.Layer.AddFileLayer(layer.Name, layertitle, dirPath + "/" + layertitle + ".mbtiles", proj.Id, layers.Count + i++);
                 }
             }
