0c183185972f069c25edf7e5081e81e24d9e34a6
dawes <Nicholas.Dawes@geotest.ch>
2024-03-19 19:43:36 +0100

    Done button added to iOS numeric keyboard

--- Mobile/BioDivCollectorXamarin/BioDivCollectorXamarin.iOS/EntryRenderer.cs
+++ Mobile/BioDivCollectorXamarin/BioDivCollectorXamarin.iOS/EntryRenderer.cs
@@ -1,1 +1,47 @@
-﻿
+﻿using System.Drawing;
+using System.Security.Policy;
+using BioDivCollectorXamarin.Controls;
+using BioDivCollectorXamarin.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace BioDivCollectorXamarin.iOS.Renderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Element == null)
+                return;
+
+            // Check only for Numeric keyboard
+            if (this.Element.Keyboard == Keyboard.Numeric)
+                this.AddDoneButton();
+        }
+
+        /// <summary>
+        /// <para>Add toolbar with Done button</para>
+        /// </summary>
+        protected void AddDoneButton()
+        {
+            var toolbar = new UIToolbar(new RectangleF(0.0f, 0.0f, 50.0f, 44.0f));
+
+            var doneButton = new UIBarButtonItem(UIBarButtonSystemItem.Done, delegate
+            {
+                this.Control.ResignFirstResponder();
+                var baseEntry = this.Element.GetType();
+                ((IEntryController)Element).SendCompleted();
+            });
+
+            toolbar.Items = new UIBarButtonItem[] {
+                new UIBarButtonItem (UIBarButtonSystemItem.FlexibleSpace),
+                doneButton
+            };
+            this.Control.InputAccessoryView = toolbar;
+        }
+    }
+}
